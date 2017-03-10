933f27d04c8352ced1bf05a9fad08df961a37473
danielle44 <danielle44@gmail.com>
2017-03-10 02:18:20 +0200

    change status function

--- parentalLeaveApp/routes/api/users_controller.js
+++ parentalLeaveApp/routes/api/users_controller.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/', index);
 router.post('/', create);
-router.post('/update_status/', updateStatus);
+router.post('/update_status/:id', updateStatus);
 
 var CREATE_FIELDS = ['username', 'first_name', 'last_name', 
 	'facebook_img_url', 'children_amount', 'city', 
@@ -22,7 +22,7 @@ function index(req, res, next) {
 function create(req, res, next) {
 	console.log('req.body  = ', req.body);
 	User.create(req.body, {fields: CREATE_FIELDS}).then(function(user) { 
-		res.send(200);
+		res.send({ id: user.dataValues.id });
 		console.log('A new user was created: ', user.dataValues); 
 	}).catch(function(err) {
 		console.log('an error occured on creating a new user: ', err);
@@ -31,6 +31,16 @@ function create(req, res, next) {
 }
 
 function updateStatus(req, res, next) {
+	var newStatus = req.body.new_status;
+	console.log('newStatus = ', newStatus);
+	//TODO: raise if newStatus null
+
+	User.findById(req.params.id).then(function(user) {
+		user.update({ status: newStatus});
+	}).catch(function(err) {
+		console.log('an error occured while trying to change status: ', err);
+		res.send(500);
+	})
 	res.send( { func: 'updateStatus'} );
 }
 
